83ef91b391600ad9ff2857aeb6ae53f727a73abc
Donald Geddes <hi@donaldgeddes.ca>
2023-04-18 14:39:53 -0700

    Add shebang

--- .install.sh
+++ .install.sh
@@ -1,4 +1,9 @@
-apt install -y zsh git lolcat
-chsh -s $(which zsh)
+#!/usr/bin/env bash 
+
+apt update && apt install -y less man-db sudo nano zsh git lolcat
+
 
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+
+chsh -s $(which zsh)
+exec /bin/zsh
