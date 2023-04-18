a442ea25b5c975983957ef4e463a7ea75de73674
Donald Geddes <hi@donaldgeddes.ca>
2023-04-18 14:40:52 -0700

    Fix

--- .install.sh
+++ .install.sh
@@ -2,8 +2,7 @@
 
 apt update && apt install -y less man-db sudo nano zsh git lolcat
 
-
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 
 chsh -s $(which zsh)
-exec /bin/zsh
+zsh -i -c
